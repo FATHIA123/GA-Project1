b1bb749b19c22f545be07794f8065ccc864b34e0
Fathia Mohammed <mohammedfo5429@gmail.com>
2019-01-10 09:29:25 -0500

    created the questions array of objects and trynna add the js to html

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+// var buton = document.getElementsByClassName("A")
+
+// buton.addEventListener("click", function() {
+//    if ( buton.innerHTML = "they why factor" ) {
+//    alert("CORRECT!")
+//    } else {
+//        alert("wRONG!!")
+
+//    }
+
+// } )
+
+var Questions = [
+{
+    question: "how old are you?",
+    answer: ["1", "2", "3", "4" ],
+    correctAnswer: "1"
+},
+{
+    question: "what's your name?",
+    answer: ["a", "b", "c", "d" ],
+    correctAnswer: "a"
+}
+
+]
+
+
+var choiceA = document.getElementsByClassName("A")
+var choiceB = document.getElementsByClassName("B")
+var choiceC = document.getElementsByClassName("C")
+var choiceD = document.getElementsByClassName("D")
