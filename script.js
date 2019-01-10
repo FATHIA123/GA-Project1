1ba37d3abe0fd41fafb5dfe15299f32442a41709
Fathia Mohammed <mohammedfo5429@gmail.com>
2019-01-10 14:27:21 -0500

    tried the DOM, but the DOM is too much too handle and too little time, manually adding everything to HTML

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-var Questions = [
+let Questions = [
 {
     question: "how many apples are there?",
     answer: ["1", "2", "3", "4" ],
@@ -14,56 +14,89 @@ var Questions = [
 
 ]
 
-console.log (Questions)
+// console.log (Questions)
 // get buttons from the html 
 
 // change the question in HTML 
 
 
-var optionList1 = document.getElementsByClassName("A")
 
 
-for (let i=0; i< Questions.length; i ++) {
-    const selector = `Q${i+1}`
-    let Qes = document.getElementById(selector)
-    var changed =  Qes.innerHTML = Questions[i].question
-    // console.log(Qes )
-    // console.log(changed)
+// var optionList2 = document.getElementsByClassName("")
+// let optionList1 = document.querySelectorAll('.O1')
+// console.log(optionList1)
 
-    // console.log(Questions[i].answer)
-    // do another loop for answers
+// for ( i=0; i< Questions.length; i ++) {
+//     const selector = `Q${i+1}`
+//     let Qes = document.getElementById(selector)
+//     var changed =  Qes.innerHTML = Questions[i].question
+//     // console.log(Qes )
+//     // console.log(changed)
+    
+//     // console.log(Questions[i].answer)
+//     // do another loop for answers
+    
+ 
 
-    for (let j = 0; j < Questions[i].answer.length; j++) {
+// }
 
-        // let Op =/
-        // console.log(Questions[i].answer[j]) 
-        // do innerhtml here
-        console.log(optionList1[j])
+// for ( i=0; i< optionList1.length; i ++) {
 
-    }
-}
+//     var currentButton = optionList1[i]
+//     console.log(currentButton)
 
+//      for (j=0 ; j < )
 
-// if (Qes.innerHTML === nullv) {
-//     Qes.innerHTML = Questions[0].question.value;
-//   } else {
-//     Qes.innerHTML =Questions[0].question.value ;
-//   }
-
-
-
-
-var optionList2 = document.getElementsByClassName("B")
-
-
+//     // const selector = `Q${i+1}`
+//     // let Qes = document.getElementById(selector)
+//     // var changed =  Qes.innerHTML = optionList1[i].question
+//     // console.log(Qes )
+//     // console.log(changed)
+    
+//     // console.log(Questions[i].answer)
+//     // do another loop for answers
+    
+ 
+    
+// }
 
 
-// get the arraay from the objects 
-for (i=0; i<Questions.length; i++) {
-var answerArrary = Questions[i].answer
-// optionList1.innerHTML = answerArrary
-console.log(answerArrary)
-}
+
+
+
+
+
+
+
+
+
+// for (let j = 0; j < Questions[i].answer.length; j++) {
+        
+//     const selectorA = `O${j+1}`
+//     var optionList2 = document.getElementsByClassName("O2")[j]
+    
+    
+//     console.log(Questions[j].answer )
+    
+    
+//     // console.log()
+//     // let Op =/
+//     // console.log(Questions[i].answer[j]) 
+//     // do innerhtml here
+//     // console.log(optionList)
+    
+//     }
+    
+//     var optionList1 = document.getElementsByClassName("O1")
+    
+//     console.log(optionList1)
+    
+    
+//     optionList1.forEach( (i, j) => { 
+//         var changeAns = optionList.innerHTML = Questions[i].answer[j]
+//     console.log(changeAns)
+// });
+
 
 
 
